b4c3a42b1de27e135c6082366196d138d410a8a8
VetleFell <vetle.stubberud@gmail.com>
2022-09-09 00:34:43 +0100

    test: added some small tests

--- hooks/useTransaction.test.ts
+++ hooks/useTransaction.test.ts
@@ -1,11 +1,55 @@
+import { AxiosError } from 'axios';
+import { userAddress } from '../common/constants';
 import useTransactions from './useTransactions';
-
+// These are mostly integration as integration is usually higher value.
+// If we go for unit tests it should be higher coverage. I love unit tests tho.
 describe('useTransaction', () => {
-  describe('returns', () => {
-    it('transaction', () => {
-      expect(
-        useTransactions(true).getTransaction('ST3Y4P66VZ7JG5QXQ8MFR8D08GGBJR0KJCT2NXN4F')
-      ).toEqual([]);
+  it('supports value comparison', () => {
+    const { getTransaction, getTestTokensFromFaucet } = useTransactions(true);
+    expect(true).toEqual(true);
+    expect(false).not.toEqual(true);
+    expect(getTransaction).not.toEqual(getTestTokensFromFaucet);
+  });
+  describe('getTransaction', () => {
+    it('initializes', async () => {
+      const { getTransaction } = useTransactions(true);
+      expect(getTransaction).not.toBeUndefined();
+    });
+    it('transaction with results', async () => {
+      const { getTransaction, getTestTokensFromFaucet } = useTransactions(true);
+      try {
+        await getTestTokensFromFaucet(userAddress);
+      } catch (e) {
+        console.log(e);
+      }
+      const data = await getTransaction(userAddress);
+      expect(data?.results).not.toBeUndefined();
+    });
+    it('error with invalid address', async () => {
+      const { getTransaction } = useTransactions(true);
+      await expect(getTransaction('jj')).rejects.toThrow();
+    });
+
+    it('returns null when signed out', async () => {
+      const { getTransaction } = useTransactions(false);
+      const data = await getTransaction(userAddress);
+      expect(data).toBeUndefined();
+    });
+  });
+  describe('getTestTokens', () => {
+    it('initializes', async () => {
+      const { getTestTokensFromFaucet } = useTransactions(true);
+      expect(getTestTokensFromFaucet).not.toBeUndefined();
+    });
+    it('error with invalid address', async () => {
+      const { getTestTokensFromFaucet } = useTransactions(true);
+      await expect(getTestTokensFromFaucet('jj')).rejects.toThrow();
+    });
+
+    it('returns null when signed out', async () => {
+      const { getTestTokensFromFaucet } = useTransactions(false);
+      const data = await getTestTokensFromFaucet(userAddress);
+      expect(data).toBeUndefined();
     });
   });
 });
--- hooks/useTransactions.ts
+++ hooks/useTransactions.ts
@@ -6,56 +6,56 @@ import {
   stacksWebSocketTestNetV1Url,
   userAddress,
 } from '../common/constants';
-
+// This is more a repository not a hook. Reason being error with tests and hooks in next.js.
+// Only difference is the memoiziation anyways, which is pretty irrelevant here
 const useTransactions = (isSignedIn: boolean) => {
-  const subscribeToTransactions = useCallback(async (testnetAddress: string) => {
+  const subscribeToTransactions = async (testnetAddress: string) => {
     const client = await connectWebSocketClient(stacksWebSocketTestNetV1Url);
 
     const sub = await client.subscribeAddressTransactions(
       testnetAddress === '' ? userAddress : testnetAddress,
       event => {
+        // The solution is to either return the new status and id then match with the object
+        // inside the transactions array or to just refetch a single transaction using the id endpoint
+        // Way too many issues to solve this one...
         console.log(event);
-        return event;
+        return { id: event.tx_id, status: event.tx_status };
       }
     );
     await sub.unsubscribe();
-  }, []);
+  };
 
-  const getTransaction = useCallback(
-    async (testnetAddress: string) => {
-      if (isSignedIn) {
-        try {
-          const response: TransactionEventsResponse = await axios
-            .get(
-              `${stacksTestNetV1APIUrl}tx/mempool?address=${
-                testnetAddress === '' ? userAddress : testnetAddress
-              }`
-            )
-            .then(res => res.data);
-          return response;
-        } catch (e) {
-          throw e;
-        }
+  const getTransaction = async (testnetAddress: string) => {
+    // All these with isSignedIn should have an else clause that handles
+    if (isSignedIn) {
+      try {
+        const response: TransactionEventsResponse = await axios
+          .get(
+            `${stacksTestNetV1APIUrl}tx/mempool?address=${
+              testnetAddress === '' ? userAddress : testnetAddress
+            }`
+          )
+          .then(res => res.data);
+        return response;
+      } catch (e) {
+        throw e;
       }
-    },
-    [isSignedIn]
-  );
+    }
+  };
 
-  const getTestTokensFromFaucet = useCallback(
-    async (testnetAddress: string) => {
-      if (isSignedIn) {
-        try {
-          const response = await axios.post(`${stacksTestNetV1APIUrl}faucets/stx`, {
-            address: testnetAddress,
-          });
-          return response.data;
-        } catch (e) {
-          throw e;
-        }
+  const getTestTokensFromFaucet = async (testnetAddress: string) => {
+    // All these with isSignedIn should have an else clause that handles
+    if (isSignedIn) {
+      try {
+        const response = await axios.post(`${stacksTestNetV1APIUrl}faucets/stx`, {
+          address: testnetAddress,
+        });
+        return response.data;
+      } catch (e) {
+        throw e;
       }
-    },
-    [isSignedIn]
-  );
+    }
+  };
 
   return { getTransaction, subscribeToTransactions, getTestTokensFromFaucet };
 };
